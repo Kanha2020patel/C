b85b3aa66bdae16d3c0352a3990f83c0b01e935c
Ranjeet <ranjeetkurmi96@gmail.com>
2024-08-14 04:13:56 +0530

    pahla commit

---
+++ bad.c
@@ -0,0 +1,12 @@
+// Author : Nitesh Kumar Patel
+// Date : 14/08/2024 
+
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int arr[5];
+    arr[7] = 55;
+    printf("arr[7] : %d\n",arr[7]);
+    return 0;
+}
---
+++ main.c
@@ -0,0 +1,9 @@
+// Author : Nitesh Kumar Patel
+// Date : 14/08/2024 
+
+#include <stdio.h>
+
+int main(int argc, char **argv){
+    printf("First Program from Mint OS(XFCE Desktop)\n");
+    return 0;
+}
